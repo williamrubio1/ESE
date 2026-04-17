5039090b3d673ed23bf8c832eac41a7e707fae26
William Ricardo Rubio Sanabria <wrubios@esemeta.com>
2026-04-17 17:58:49 -0500

    Quitamos a AIEnda

--- AIEnda/agenda/frontend/src/api/auth.api.js
+++
@@ -1,8 +0,0 @@
-// src/api/auth.api.js
-
-import api from './axios';
-
-export const loginApi              = (data)  => api.post('/auth/login', data);
-export const cambiarContrasenaApi  = (data)  => api.post('/auth/cambiar-contrasena', data);
-export const recuperarContrasenaApi = (data) => api.post('/auth/recuperar', data);
-export const obtenerContrasenaApi  = (id)    => api.get(`/auth/contrasena/${id}`);
--- AIEnda/agenda/frontend/src/api/disponibilidad.api.js
+++
@@ -1,8 +0,0 @@
-// src/api/disponibilidad.api.js
-
-import api from './axios';
-
-export const turnosLibresApi    = (medicoId, fecha) =>
-  api.get('/disponibilidad', { params: { medicoId, fecha } });
-
-export const espaciosVaciosApi  = () => api.get('/disponibilidad/espacios-vacios');
